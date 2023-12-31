5d44770a3e6164dc94e2fb6bd588e31cad8b7e67
rafiki1990 <blaiserafiki84@gmail.com>
2023-12-31 16:01:45 +0200

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,15 +2,16 @@
 """Starts a flask web application.
 """
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-#define the route for the root URL '/'
+# Define the route from the root URL '/'
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB! """
+    """Displays 'Hello HBNB!'. """
     return "Hello HBNB!"
 
+# Define the route for '/hbnb'
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'. """
@@ -24,7 +25,19 @@ def c_with_text(text):
     formatted_text = text.replace('_', ' ')
     return "c {}".format(formatted_text)
 
+# Define the route for '/python/(<text>)'
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_with_text(text):
+    """Displays 'python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with spaces.
+    """
+    # Replace underscores with spaces in the text variable
+    formatted_text = text.replace('_', ' ')
+    return "python {}".format(formatted_text)
+
 if __name__ == "__main__":
-    # Start the flask development server 
+    # Start the flask development server
     # Listen on all available network interfaces (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000)
